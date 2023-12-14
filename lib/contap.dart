cd415ad1ac316f83e07fd79563c590731d53b7d5
Lenovo <sa1dumarxon.od1lovv@gmail.com>
2023-12-14 20:39:08 +0500

    A

--- lib/contap.dart
+++ lib/contap.dart
@@ -17,12 +17,6 @@ class _ContapState extends State<Contap> {
   bool _icon = false;
   int _selectedIndex = 0;
 
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Search Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Profile Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-  ];
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;

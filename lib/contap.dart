923c9b3b4836c29a7652a4e7e33f0039350da4ab
Lenovo <sa1dumarxon.od1lovv@gmail.com>
2023-12-14 20:49:49 +0500

    A

--- lib/contap.dart
+++ lib/contap.dart
@@ -3,6 +3,7 @@ import 'package:flutter_mashgulot/MyApp.dart';
 import 'package:flutter_mashgulot/add.dart';
 import 'package:flutter_mashgulot/ended/end2.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
 class Contap extends StatefulWidget {
   final int? index;
 
@@ -15,14 +16,6 @@ class Contap extends StatefulWidget {
 class _ContapState extends State<Contap> {
   TextEditingController textEditingController = TextEditingController();
   bool _icon = false;
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
 
   @override
   void initState() {
@@ -36,135 +29,127 @@ class _ContapState extends State<Contap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[700],
-      extendBody: true,
-      body:
-      Column(
-        children: [
-          Expanded(
-            child: ListView(
-              physics: const ClampingScrollPhysics(),
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      bottomRight: Radius.circular(25),
-                      bottomLeft: Radius.circular(25),
+        backgroundColor: Colors.yellow[700],
+        extendBody: true,
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                physics: const ClampingScrollPhysics(),
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        bottomRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25),
+                      ),
+                      color: Colors.yellow[300],
                     ),
-                    color: Colors.yellow[300],
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: IconButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                icon: const Icon(Icons.arrow_back_ios)),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: IconButton(
-                              onPressed: () {
-                                if (_icon == true) {
-                                  setState(() {
-                                    _icon = false;
-                                  });
-                                } else {
-                                  setState(() {
-                                    _icon = true;
-                                  });
-                                }
-                              },
-                              icon: Icon((_icon == false)
-                                  ? AntDesign.staro
-                                  : AntDesign.star),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: IconButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(Icons.arrow_back_ios)),
                             ),
-                          ),
-                        ],
-                      ),
-                      TextField(
-                        controller: textEditingController,
-                        textAlignVertical: TextAlignVertical.center,
-                        minLines: 1,
-                        maxLines: null,
-                        readOnly: true,
-                        keyboardType: TextInputType.multiline,
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (_) => Add(
-                                    index: widget.index,
-                                  )));
-                        },
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(vertical: 70),
-                          hintText: "  Yozuv",
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: IconButton(
+                                onPressed: () {
+                                  if (_icon == true) {
+                                    setState(() {
+                                      _icon = false;
+                                    });
+                                  } else {
+                                    setState(() {
+                                      _icon = true;
+                                    });
+                                  }
+                                },
+                                icon: Icon((_icon == false)
+                                    ? AntDesign.staro
+                                    : AntDesign.star),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                        TextField(
+                          controller: textEditingController,
+                          textAlignVertical: TextAlignVertical.center,
+                          minLines: 1,
+                          maxLines: null,
+                          readOnly: true,
+                          keyboardType: TextInputType.multiline,
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => Add(
+                                          index: widget.index,
+                                        )));
+                          },
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(vertical: 70),
+                            hintText: "  Yozuv",
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => EndIkki(
+                                  index: widget.index,
+                                )));
+                  },
+                  child: const Column(
+                    children: [Icon(Icons.done_outline)],
                   ),
-                )
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => Add(
+                                  index: widget.index,
+                                )));
+                  },
+                  child: const Column(
+                    children: [Icon(Icons.done_outline)],
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      textBox.deleteAt(widget.index!);
+                      textEditingController.clear();
+                      Navigator.pop(context);
+                    });
+                  },
+                  child: const Column(
+                    children: [Icon(Icons.done_outline)],
+                  ),
+                ),
               ],
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.yellow[300],
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => EndIkki(
-                          index: widget.index,
-                        )));
-              },
-              child: const Icon(Icons.done_outline),
-            ),
-            label: "Yakunlash",),
-          BottomNavigationBarItem(
-            icon: MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => Add(
-                          index: widget.index,
-                        )));
-              },
-              child: const Icon(MaterialCommunityIcons.pen),
-            ),
-            label: "Tahrirlash",
-          ),
-          BottomNavigationBarItem(
-            icon: MaterialButton(
-                onPressed: () {
-                  setState(() {
-                    textBox.deleteAt(widget.index!);
-                    textEditingController.clear();
-                    Navigator.pop(context);
-                  });
-                },
-                child: const Icon(
-                  AntDesign.delete,
-                )),
-            label: "O'chirish",
-          ),
-        ], currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
+            ))
+          ],
+        ));
   }
 }
